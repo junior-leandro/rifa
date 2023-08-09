890f15b5bd1ba8c09c07814ce1c71078fa2e002a
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-08-09 12:01:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,125 @@
+
+const num00 = document.getElementById('00').innerText;
+const num01 = document.getElementById('01').innerText;
+const num02 = document.getElementById('02').innerText;
+const num03 = document.getElementById('03').innerText;
+const num04 = document.getElementById('04').innerText;
+const num05 = document.getElementById('05').innerText;
+const num06 = document.getElementById('06').innerText;
+const num07 = document.getElementById('07').innerText;
+const num08 = document.getElementById('08').innerText;
+const num09 = document.getElementById('09').innerText;
+const num10 = document.getElementById('10').innerText;
+const num11 = document.getElementById('11').innerText;
+const num12 = document.getElementById('12').innerText;
+const num13 = document.getElementById('13').innerText;
+const num14 = document.getElementById('14').innerText;
+const num15 = document.getElementById('15').innerText;
+const num16 = document.getElementById('16').innerText;
+const num17 = document.getElementById('17').innerText;
+const num18 = document.getElementById('18').innerText;
+const num19 = document.getElementById('19').innerText;
+const num20 = document.getElementById('20').innerText;
+const num21 = document.getElementById('21').innerText;
+const num22 = document.getElementById('22').innerText;
+const num23 = document.getElementById('23').innerText;
+const num24 = document.getElementById('24').innerText;
+const num25 = document.getElementById('25').innerText;
+const num26 = document.getElementById('26').innerText;
+const num27 = document.getElementById('27').innerText;
+const num28 = document.getElementById('28').innerText;
+const num29 = document.getElementById('29').innerText;
+const num30 = document.getElementById('30').innerText;
+const num31 = document.getElementById('31').innerText;
+const num32 = document.getElementById('32').innerText;
+const num33 = document.getElementById('33').innerText;
+const num34 = document.getElementById('34').innerText;
+const num35 = document.getElementById('35').innerText;
+const num36 = document.getElementById('36').innerText;
+const num37 = document.getElementById('37').innerText;
+const num38 = document.getElementById('38').innerText;
+const num39 = document.getElementById('39').innerText;
+const num40 = document.getElementById('40').innerText;
+const num41 = document.getElementById('41').innerText;
+const num42 = document.getElementById('42').innerText;
+const num43 = document.getElementById('43').innerText;
+const num44 = document.getElementById('44').innerText;
+const num45 = document.getElementById('45').innerText;
+const num46 = document.getElementById('46').innerText;
+const num47 = document.getElementById('47').innerText;
+const num48 = document.getElementById('48').innerText;
+const num49 = document.getElementById('49').innerText;
+const num50 = document.getElementById('50').innerText;
+const num51 = document.getElementById('51').innerText;
+const num52 = document.getElementById('52').innerText;
+const num53 = document.getElementById('53').innerText;
+const num54 = document.getElementById('34').innerText;
+const num55 = document.getElementById('55').innerText;
+const num56 = document.getElementById('56').innerText;
+const num57 = document.getElementById('57').innerText;
+const num58 = document.getElementById('58').innerText;
+const num59 = document.getElementById('59').innerText;
+const num60 = document.getElementById('60').innerText;
+const num61 = document.getElementById('61').innerText;
+const num62 = document.getElementById('62').innerText;
+const num63 = document.getElementById('63').innerText;
+const num64 = document.getElementById('64').innerText;
+const num65 = document.getElementById('65').innerText;
+const num66 = document.getElementById('66').innerText;
+const num67 = document.getElementById('67').innerText;
+const num68 = document.getElementById('68').innerText;
+const num69 = document.getElementById('69').innerText;
+const num70 = document.getElementById('70').innerText;
+const num71 = document.getElementById('71').innerText;
+const num72 = document.getElementById('72').innerText;
+const num73 = document.getElementById('73').innerText;
+const num74 = document.getElementById('74').innerText;
+const num75 = document.getElementById('75').innerText;
+const num76 = document.getElementById('76').innerText;
+const num77 = document.getElementById('77').innerText;
+const num78 = document.getElementById('78').innerText;
+const num79 = document.getElementById('79').innerText;
+const num80 = document.getElementById('80').innerText;
+const num81 = document.getElementById('81').innerText;
+const num82 = document.getElementById('82').innerText;
+const num83 = document.getElementById('83').innerText;
+const num84 = document.getElementById('84').innerText;
+const num85 = document.getElementById('85').innerText;
+const num86 = document.getElementById('86').innerText;
+const num87 = document.getElementById('87').innerText;
+const num88 = document.getElementById('88').innerText;
+const num89 = document.getElementById('89').innerText;
+const num90 = document.getElementById('90').innerText;
+const num91 = document.getElementById('91').innerText;
+const num92 = document.getElementById('92').innerText;
+const num93 = document.getElementById('93').innerText;
+const num94 = document.getElementById('94').innerText;
+const num95 = document.getElementById('95').innerText;
+const num96 = document.getElementById('96').innerText;
+const num97 = document.getElementById('97').innerText;
+const num98 = document.getElementById('98').innerText;
+const num99 = document.getElementById('99').innerText;
+
+
+
+    function NumerosEscolhidos() {
+
+        let vendidos = [
+            '01', 
+            '99',
+            '79',
+            '10',
+        
+        ]
+
+        for (let i = 0; i < vendidos.length ; i++) {
+            console.log(i);
+            console.log(vendidos)
+            /*
+            let numeroatual = 
+
+            if (vendidos[i] == numeroatual)
+*/
+    }
+    }
+NumerosEscolhidos();
